ddbe6534ba24821b549f50d0244ac26f2557410f
Vania <Vania.Nathania@ardyanda.id>
2026-07-01 00:24:51 +0700

    update icon

--- src/layouts/multiTab/blocks/main.tsx
+++ src/layouts/multiTab/blocks/main.tsx
@@ -23,7 +23,7 @@ export const Main = () => {
   return (
     <Fragment>
       <Helmet>
-        <title>Sinorita | {activeTab}</title>
+        <title>{activeTab}</title>
       </Helmet>
       <div className="grow relative flex flex-col">
         <Header />
